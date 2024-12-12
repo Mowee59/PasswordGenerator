ffb4a73a34d337cc01cb5bd2f1383788fa2e4665
Aniss <aniss.mahfoudi@gmail.com>
2024-12-12 14:37:33 +0100

    Created and styled checkboxes and their labels

--- src/app/component/password-generator/password-generator.component.ts
+++ src/app/component/password-generator/password-generator.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PasswordDisplayComponent } from '../password-display/password-display.component';
+import { PasswordOptionComponent } from '../password-option/password-option.component';
 
 @Component({
   selector: 'app-password-generator',
   standalone: true,
-  imports: [PasswordDisplayComponent],
+  imports: [PasswordDisplayComponent, PasswordOptionComponent],
   templateUrl: './password-generator.component.html',
   styleUrl: './password-generator.component.css',
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
       fontFamily: {
         jetBrainsMono: ["JetBrains Mono", "serif"],
       },
+      colors: {
+        "background-page": "#18171F",
+        "background-component": "#24232C",
+        "text-dark": "#817D92",
+        "text-light": "#E6E5EA",
+        accent: "#A4FFAF",
+        error: "#F64A4A",
+        warning: "#FB7C58",
+        caution: "#F8CD65",
+      },
     },
   },
   plugins: [],
